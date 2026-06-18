bdc11fb88840029aa1decb77bd15133353d6bee8
TrongDuc <ducaz4321@gmail.com>
2026-06-18 20:51:38 +0700

    feat: dev A tinh gpa theo tuyen tinh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def tinh_diem_gpa(diem_so)
-  #TODO: Cần cập nhật logic quy đổi sang hệ số 4
-  return 0.0
+def tinh_diem_gpa(diem_so):
+  #Cong thuc tuyen tinh don gian cua DEV A
+  return round((diem_so / 10)*4,2)
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))  
